a8c1d59e5367ce4184dd1e952b79a70f46ddc5d9
Andrey Dolgov <drus.dolgov@gmail.com>
2024-03-24 21:34:20 +0300

    update

--- lib/ModelTrainer.h
+++ lib/ModelTrainer.h
@@ -6,7 +6,6 @@
 #include <string>
 #include <memory>
 #include "tensorflow/lite/interpreter.h"
-#include "tensorflow/lite/delegates/flex/delegate.h"
 #include "tensorflow/lite/model.h"
 
 class ModelTrainer {
